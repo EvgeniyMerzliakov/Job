7be441d2458949317bbbe6283ce8b186ae2f960e
Evgeniy <evgeniy_mq@mail.com>
2023-03-20 11:31:01 +0300

    Метод для отображения массива и применение этих методов

--- Code/Program.cs
+++ Code/Program.cs
@@ -21,3 +21,20 @@ void SecondArrayWithIF(string[] array1, string[] array2)
         }
     }
 }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+SecondArrayWithIF(array1, array2);
+Console.WriteLine("Первый массив");
+PrintArray(array1);
+Console.WriteLine();
+Console.WriteLine("Второй массив");
+PrintArray(array2);
