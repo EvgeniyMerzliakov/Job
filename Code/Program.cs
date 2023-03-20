ca26d6a82bb069a643cec709591ed4e49ef4b91f
Evgeniy <evgeniy_mq@mail.com>
2023-03-20 11:29:50 +0300

    Создал два массива

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.WriteLine("Введите размер массива");
+int x = Convert.ToInt32(Console.ReadLine());
+string[] array1 = new string[x];
+for (int i = 0; i < x; i++)
+{
+  Console.WriteLine("Напишите элемент массива");
+  string y = Console.ReadLine();
+  array1[i] = y;
+}
+string[] array2 = new string[array1.Length];
